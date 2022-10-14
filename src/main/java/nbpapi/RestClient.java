d323647c8ce66719a033ff19040343010be5c60c
poe-cat <katarzyna.tramea@gmail.com>
2022-10-14 12:14:54 +0200

    Refactor code, create separate RestClient class, add regex to date validation

--- src/main/java/nbpapi/RestClient.java
+++ src/main/java/nbpapi/RestClient.java
@@ -7,46 +7,13 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Scanner;
 
 public class RestClient {
 
     private static final String NBP_API_URL = "http://api.nbp.pl/api/exchangerates/rates/a/usd/";
     private static String jsonUrl;
 
-    public static void main(String[] args) throws IOException {
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Podaj nazwę produktu: ");
-        String nazwa = scanner.nextLine();
-        System.out.println("Podaj datę księgowania: ");
-        String data = scanner.nextLine();
-        System.out.println("Podaj cenę w USD: ");
-        double cenaUSD = scanner.nextDouble();
-        scanner.nextLine();
-
-        double cenaPLN = calculateUsdToPLN(data) * cenaUSD;
-
-        System.out.println("Dodałeś produkt.\nNazwa: " + nazwa + ", data księgowania: "
-                + data + ", cena w USD: " + cenaUSD + ", cena w PLN: " + cenaPLN);
-
-
-
-        Inventory inventory = new Inventory();
-        inventory.addProduct(nazwa, data, cenaUSD, cenaPLN);
-
-        List<Product> products = inventory.selectProducts();
-
-        System.out.println("\nLista wszystkich produktów: ");
-        for(Product p: products)
-            System.out.println(p);
-
-        inventory.closeConnection();
-    }
-
-    static double calculateUsdToPLN(String date, double price) throws IOException {
+    public double calculateUsdToPLN(String date, double price) throws IOException {
 
         double exchangedRate;
 
@@ -64,7 +31,7 @@ public class RestClient {
         return exchangedRate;
     }
 
-    static double calculateUsdToPLN(String date) throws IOException {
+    public double calculateUsdToPLN(String date) throws IOException {
 
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate localDate = LocalDate.parse(date, dateTimeFormatter);
@@ -79,5 +46,4 @@ public class RestClient {
 
         return usdRate;
     }
-
 }
