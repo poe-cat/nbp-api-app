555e3019a272b9dd0da66615803b6a46b819f374
poe-cat <katarzyna.tramea@gmail.com>
2022-10-14 20:00:24 +0200

    Refactor code (one extra method)

--- src/main/java/nbpapi/RestClient.java
+++ src/main/java/nbpapi/RestClient.java
@@ -13,6 +13,24 @@ public class RestClient {
     private static final String NBP_API_URL = "http://api.nbp.pl/api/exchangerates/rates/a/usd/";
     private static String jsonUrl;
 
+
+    public double calculateUsdToPLN(String date) throws IOException {
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate localDate = LocalDate.parse(date, dateTimeFormatter);
+        String formatDate = dateTimeFormatter.format(localDate);
+
+        jsonUrl = NBP_API_URL + formatDate;
+        URL url = new URL(jsonUrl);
+        InputStreamReader reader = new InputStreamReader(url.openStream());
+
+        Rate rate = new Gson().fromJson(reader, Rate.class);
+        double usdRate = rate.getRates().get(0).getMid();
+
+        return usdRate;
+    }
+
+
     public double calculateUsdToPLN(String date, double price) throws IOException {
 
         double exchangedRate;
@@ -29,21 +47,5 @@ public class RestClient {
         exchangedRate = rate.getRates().get(0).getMid() * price;
 
         return exchangedRate;
-    }
-
-    public double calculateUsdToPLN(String date) throws IOException {
-
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate localDate = LocalDate.parse(date, dateTimeFormatter);
-        String formatDate = dateTimeFormatter.format(localDate);
-
-        jsonUrl = NBP_API_URL + formatDate;
-        URL url = new URL(jsonUrl);
-        InputStreamReader reader = new InputStreamReader(url.openStream());
-
-        Rate rate = new Gson().fromJson(reader, Rate.class);
-        double usdRate = rate.getRates().get(0).getMid();
-
-        return usdRate;
     }
 }
